a1a27d939b9d8c8d64c91fed8e2029ca890d1624
georgegp8 <georgegp8@users.noreply.github.com>
2025-11-20 20:43:22 -0500

    Initial commit - SEO optimized Next.js project with meta tags, sitemap and robots.txt

---
+++ pages/api/sitemap.js
@@ -0,0 +1,17 @@
+// GUERRA PACHECO GEORGE MIKY -5C24B
+const BASE_URL = "https://mi-sitio.com";
+
+export default async function handler(req, res) {
+  const urls = ["/", "/blog", "/contacto"]; 
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls
+      .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
+      .join("")}
+  </urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+}
